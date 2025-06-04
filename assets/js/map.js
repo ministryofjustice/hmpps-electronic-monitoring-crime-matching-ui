6d788e2454025f688815adea4d43451605d9aa3d
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2025-06-04 10:58:49 +0100

    Reverting map change

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,15 +1,14 @@
 import axios from 'axios'
-import { Feature, Map, View } from 'ol'
+import { Map, View } from 'ol'
 import TileState from 'ol/TileState'
 import { buffer, boundingExtent } from 'ol/extent'
 import { GeoJSON } from 'ol/format'
-import { Point } from 'ol/geom'
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
 import LayerGroup from 'ol/layer/Group'
 import { fromLonLat, transformExtent } from 'ol/proj'
 import VectorSource from 'ol/source/Vector'
 import XYZ from 'ol/source/XYZ'
-import { Circle as CircleStyle, Fill, Stroke, Style, Text, RegularShape } from 'ol/style'
+import { Circle as CircleStyle, Fill, Stroke, Style, Text } from 'ol/style'
 
 function MapComponent($module) {
   this.cacheEls($module)
@@ -40,25 +39,20 @@ MapComponent.prototype = {
     this.tileUrl = $module.getAttribute('data-tile-url')
 
     this.lineSource = new VectorSource()
-    this.arrowSource = new VectorSource()
     this.pointSource = new VectorSource()
-    this.confidenceSource = new VectorSource()
   },
 
   render() {
     this.renderMap()
     this.appendPoints()
     this.appendLines()
-    this.appendArrows()
   },
 
   createControls() {
     this.pointsToggle = document.querySelector('#locations')
-    this.confidenceToggle = document.querySelector('#confidence')
     this.linesToggle = document.querySelector('#tracks')
 
     this.pointsToggle.onchange = () => this.togglePoints()
-    this.confidenceToggle.onchange = () => this.toggleConfidence()
     this.linesToggle.onchange = () => this.toggleLines()
   },
 
@@ -68,10 +62,6 @@ MapComponent.prototype = {
 
   togglePoints() {
     this.$pointsLayer.setVisible(!this.$pointsLayer.getVisible())
-  },
-
-  toggleConfidence() {
-    this.$confidenceLayer.setVisible(!this.$confidenceLayer.getVisible())
   },
 
   toggleLines() {
@@ -113,16 +103,6 @@ MapComponent.prototype = {
       ],
     })
 
-    this.$confidenceLayer = new LayerGroup({
-      title: 'Confidence',
-      layers: [
-        new VectorLayer({
-          source: this.pointSource,
-          style: this.confidenceCircleStyle.bind(this),
-        }),
-      ],
-    })
-
     this.$linesLayer = new LayerGroup({
       title: 'Lines',
       layers: [
@@ -130,14 +110,6 @@ MapComponent.prototype = {
           source: this.lineSource,
           style: this.lineStyle.bind(this),
         }),
-        new VectorLayer({
-          source: this.arrowSource,
-        }),
-        // new VectorLayer({
-        //   source: new VectorSource({
-        //     features: arrowFeatures,
-        //   }),
-        // }),
       ],
     })
 
@@ -152,7 +124,6 @@ MapComponent.prototype = {
         }),
         this.$linesLayer,
         this.$pointsLayer,
-        this.$confidenceLayer,
       ],
       view: new View({
         projection: 'EPSG:3857',
@@ -163,47 +134,6 @@ MapComponent.prototype = {
         zoom: 13,
       }),
     })
-  },
-
-  generateArrowFeatures(lineFeatures) {
-    const arrowFeatures = []
-
-    lineFeatures.forEach((lineFeature) => {
-    //   const geometry = lineFeature.getGeometry()
-    //   if (!geometry || geometry.getType() !== 'LineString') return
-
-    //   const coords = geometry.getCoordinates()
-
-    //   for (let i = 0; i < coords.length - 1; i++) {
-    //     const start = coords[i]
-    //     const end = coords[i + 1]
-
-    //     const dx = end[0] - start[0]
-    //     const dy = end[0] - start[0]
-    //     const rotation = Math.atan2(dy, dx)
-
-    //     const midX = (start[0] + end[0]) / 2
-    //     const midY = (start[1] + end[1]) / 2
-
-    //     const arrowFeature = new Feature({
-    //       geometry: new Point([midX, midY]),
-    //     })
-
-    //     arrowFeature.setStyle(
-    //       new Style({
-    //         image: new RegularShape({
-    //           points: 3,
-    //           radius: 8,
-    //           fill: new Fill({ color: 'blue' }),
-    //           rotation: -rotation,
-    //           rotateWithView: true,
-    //         }),
-    //       }) 
-    //     )
-    //     arrowFeatures.push(arrowFeature)
-    //   }
-    })
-    // return arrowFeatures
   },
 
   appendPoints() {
@@ -251,29 +181,12 @@ MapComponent.prototype = {
     }
   },
 
-  appendArrows() {
-    const lineFeatures = this.lineSource
-    const arrowFeatures = this.generateArrowFeatures(lineFeatures)
-    // this.arrowSource.addFeatures(arrowFeatures)
-    // this.arrowSource.features = arrowFeatures
-  },
-
   lineStyle() {
     return new Style({
       stroke: new Stroke({
         width: 2,
         color: 'black',
       }),
-    })
-  },
-
-  confidenceCircleStyle(feature) {
-    return new Style({
-      image: new CircleStyle({
-        radius: feature.get('confidence'),
-        stroke: new Stroke({ color: 'orange', width: 2 }),
-      }),
-      text: this.textStyle(feature),
     })
   },
 
