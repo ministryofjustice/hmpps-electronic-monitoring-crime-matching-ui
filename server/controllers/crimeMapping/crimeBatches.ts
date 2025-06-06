71a350275d61e9edf4b1a6caa07687b519427e69
Ben Carter <ben.carter@justice.gov.uk>
2025-06-06 10:49:10 +0100

    WIP

--- server/controllers/crimeMapping/crimeBatches.ts
+++ server/controllers/crimeMapping/crimeBatches.ts
@@ -1,9 +1,21 @@
 import { RequestHandler } from 'express'
+import z from 'zod'
+
+const crimeBatchesQuerySchema = z.object({
+  searchId: z.string().optional()
+})
 
 export default class CrimeBatchesController {
-  constructor() {}
+  constructor(private readonly service: CrimeBatchesService) {}
+
 
   view: RequestHandler = async (req, res) => {
-    res.render('pages/crime-mapping/crimeBatches')
+    const query = crimeBatchesQuerySchema.parse(req.query)
+    const queryExecutionId = query.searchId
+    const queryResults = await this.service.getResults(queryExecutionId)
+
+    res.render('pages/crime-mapping/crimeBatches', {
+      crimeBatches: queryResults.crimeBatches
+    })
   }
 }
