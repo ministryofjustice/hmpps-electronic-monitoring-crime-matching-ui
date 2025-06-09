b8549e6dc31ba8382a916f3cd520a87ae29db6ca
Ben Carter <ben.carter@justice.gov.uk>
2025-06-09 09:06:35 +0100

    WIP

--- server/controllers/crimeMapping/crimeBatches.ts
+++ server/controllers/crimeMapping/crimeBatches.ts
@@ -1,21 +1,32 @@
 import { RequestHandler } from 'express'
-import z from 'zod'
+import { z } from 'zod'
+import CrimeBatchesService from '../../services/crimeMapping/crimeBatches'
 
 const crimeBatchesQuerySchema = z.object({
-  searchId: z.string().optional()
+  searchId: z.string().optional(),
+})
+
+const crimeBatchesFormData = z.object({
+  term: z.string()
 })
 
 export default class CrimeBatchesController {
   constructor(private readonly service: CrimeBatchesService) {}
 
-
   view: RequestHandler = async (req, res) => {
+    const token = res.locals.user.token
     const query = crimeBatchesQuerySchema.parse(req.query)
     const queryExecutionId = query.searchId
-    const queryResults = await this.service.getResults(queryExecutionId)
+    const queryResults = await this.service.getResults(token, queryExecutionId)
 
     res.render('pages/crime-mapping/crimeBatches', {
-      crimeBatches: queryResults.crimeBatches
+      crimeBatches: queryResults.data,
     })
+  }
+
+  search: RequestHandler = async (req, res) => {
+    const formData = crimeBatchesFormData.parse(req.body)
+
+    res.redirect(`/crime-mapping/crime-batches?search_id=${encodeURIComponent()}`)
   }
 }
--- server/services/crimeMapping/crimeBatches.ts
+++ server/services/crimeMapping/crimeBatches.ts
@@ -1,37 +1,39 @@
 import { asUser, RestClient } from '@ministryofjustice/hmpps-rest-client'
-import logger from '../../../logger'
 import { z } from 'zod'
 
 type CrimeBatch = {
-    prisonName: string
+  prisonName: string
 }
 
 type CrimeBatchesSearchResult = {
-    data: Array<CrimeBatch>
+  data: Array<CrimeBatch>
 }
 
 const crimeBatchSearchResult = z.object({
-    data: z.array(z.object({
-        prisonName: z.string()
-    }))
+  data: z.array(
+    z.object({
+      prisonName: z.string(),
+    }),
+  ),
 })
 
-type a = z.infer<typeof crimeBatchSearchResult>
-
 export default class CrimeBatchesService {
-    constructor(private readonly crimeMatchingApiClient: RestClient) {}
+  constructor(private readonly crimeMatchingApiClient: RestClient) {}
 
-    async getResults(userToken: string, queryId?: string): Promise<CrimeBatchesSearchResult> {
-        if (queryId === undefined) {
-            return {
-                data: []
-            }
-        }
+  async getResults(userToken: string, queryId?: string): Promise<CrimeBatchesSearchResult> {
+    if (queryId === undefined) {
+      return {
+        data: [],
+      }
+    }
 
-        const response = await this.crimeMatchingApiClient.get({
-            path: `crime-batchs?id=${queryId}`
-        }, asUser(userToken))
+    const response = await this.crimeMatchingApiClient.get(
+      {
+        path: `crime-batchs?id=${queryId}`,
+      },
+      asUser(userToken),
+    )
 
-        return crimeBatchSearchResult.parse(response)
-     }
+    return crimeBatchSearchResult.parse(response)
+  }
 }
