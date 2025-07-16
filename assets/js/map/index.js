7f9f25300932352f5d739f33cf9ce02f4c781277
Ben Carter <ben.carter@justice.gov.uk>
2025-07-16 13:32:01 +0100

    Add overlays and click interactions

--- assets/js/map/index.js
+++ assets/js/map/index.js
@@ -2,23 +2,14 @@ import { Map } from 'ol'
 import OrdnanceSurveyTileLayer from './tiles'
 import DefaultView from './view'
 
-// The map class is not responsible for creating the access token
-class ElectronicMonitoringMap {
-  constructor(target, tileUrl, token) {
-    this.target = target
-    this.layers = [new OrdnanceSurveyTileLayer(tileUrl, token)]
-    this.view = new DefaultView()
-  }
-
-  addLayer(layer) {
-    this.layers.push(layer)
-  }
-
-  render() {
-    this.olMap = new Map({
-      target: this.target,
-      layers: this.layers,
-      view: this.view,
+class ElectronicMonitoringMap extends Map {
+  constructor({ target, osMapsTileUrl, osMapsAccessToken, layers = [], overlays = [], interactions = [] }) {
+    super({
+      target,
+      layers: [new OrdnanceSurveyTileLayer(osMapsTileUrl, osMapsAccessToken), ...layers],
+      overlays,
+      interactions,
+      view: new DefaultView(),
     })
   }
 }
--- assets/js/views/contextual-report/index.js
+++ assets/js/views/contextual-report/index.js
@@ -4,44 +4,59 @@ import LocationsLayer from './layers/locations'
 import TracksLayer from './layers/tracks'
 import ConfidenceLayer from './layers/confidence'
 import createLayerVisibilityToggle from './controls/layerVisibilityToggle'
-import LocationHover from './interactions/locationHover'
+import LocationPointerInteraction from './interactions/locationPointer'
+import LocationMetadataOverlay from './overlays/locationMetadata'
 
-const getToken = () => {
+const getAccessToken = () => {
   return axios.get('/map/token').then(response => response.data.access_token)
 }
 
 const initialiseContextualReportView = async () => {
-  console.log('initialise')
-
   const el = document.querySelector('[data-module="app-map"]')
+  const overlay = el.querySelector('.app-map__overlay')
+  const overlayTemplate = el.querySelector('#map-overlay-template')
+
+  // Parse data from element
   const points = JSON.parse(el.getAttribute('data-points'))
   const lines = JSON.parse(el.getAttribute('data-lines'))
   const tileUrl = el.getAttribute('data-tile-url')
-  const token = await getToken()
 
+  const token = await getAccessToken()
+
+  // Build the map
   const locationsLayer = new LocationsLayer(points)
   const tracksLayer = new TracksLayer(lines)
   const confidenceLayer = new ConfidenceLayer(points)
+  const locationMetadataOverlay = new LocationMetadataOverlay(overlay, overlayTemplate)
 
-  const map = new ElectronicMonitoringMap('app-map', tileUrl, token)
-
-  map.addLayer(locationsLayer)
-  map.addLayer(tracksLayer)
-  map.addLayer(confidenceLayer)
-  map.render()
-
-  // Focus on geolocation data
-  map.olMap.getView().fit(locationsLayer.getSource().getExtent(), {
-    maxZoom: 16,
-    padding: [30, 30, 30, 30],
-    size: map.olMap.getSize(),
+  const map = new ElectronicMonitoringMap({
+    target: 'app-map', 
+    osMapsTileUrl: tileUrl, 
+    osMapsAccessToken: token,
+    layers: [
+      locationsLayer,
+      tracksLayer,
+      confidenceLayer
+    ],
+    overlays: [
+      locationMetadataOverlay
+    ],
+    interactions: [
+      new LocationPointerInteraction(locationMetadataOverlay)
+    ]
   })
 
+  // Focus on geolocation data
+  map.getView().fit(locationsLayer.getSource().getExtent(), {
+    maxZoom: 16,
+    padding: [30, 30, 30, 30],
+    size: map.getSize(),
+  })
+
+  // Add controls
   createLayerVisibilityToggle('#locations', locationsLayer)
   createLayerVisibilityToggle('#tracks', tracksLayer)
   createLayerVisibilityToggle('#confidence', confidenceLayer)
-
-  map.olMap.addInteraction(new LocationHover())
 }
 
 export default initialiseContextualReportView
