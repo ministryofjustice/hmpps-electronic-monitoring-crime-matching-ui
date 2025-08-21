18ec04dd69ff6306c944b456048f01e017b7946b
Ben Carter <ben.carter@justice.gov.uk>
2025-08-21 14:12:23 +0100

    Add test cases for more formats

--- server/schemas/formInput/dateTime.test.ts
+++ server/schemas/formInput/dateTime.test.ts
@@ -27,14 +27,28 @@ describe('DateTimeInputModel', () => {
     expect(result.data?.toISOString()).toBe('2025-06-01T09:00:00.000Z')
   })
 
-  it('should parse a valid date using D/M/YYYY format', () => {
-    const result = DateTimeInputModel.safeParse({ date: '2/1/2025', hour: '0', minute: '0', second: '0' })
+  it('should parse a valid date using D/M/YYYY H:m:s format', () => {
+    const result = DateTimeInputModel.safeParse({ date: '1/1/2025', hour: '0', minute: '1', second: '2' })
 
     expect(result.success).toBe(true)
-    expect(result.data?.toISOString()).toBe('2025-01-02T00:00:00.000Z')
+    expect(result.data?.toISOString()).toBe('2025-01-01T00:01:02.000Z')
   })
 
-  it('should parse a valid date time using HH:mm:ss format', () => {
+  it('should parse a valid date using D/M/YYYY HH:mm:ss format', () => {
+    const result = DateTimeInputModel.safeParse({ date: '1/1/2025', hour: '00', minute: '01', second: '02' })
+
+    expect(result.success).toBe(true)
+    expect(result.data?.toISOString()).toBe('2025-01-01T00:01:02.000Z')
+  })
+
+  it('should parse a valid date using DD/MM/YYYY H:m:s format', () => {
+    const result = DateTimeInputModel.safeParse({ date: '01/01/2025', hour: '0', minute: '1', second: '2' })
+
+    expect(result.success).toBe(true)
+    expect(result.data?.toISOString()).toBe('2025-01-01T00:01:02.000Z')
+  })
+
+  it('should parse a valid date using DD/MM/YYYY HH:mm:ss format', () => {
     const result = DateTimeInputModel.safeParse({ date: '01/01/2025', hour: '00', minute: '01', second: '02' })
 
     expect(result.success).toBe(true)
