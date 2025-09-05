d234dc15dd19b9d03d72e94104ed288d249610d6
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2025-09-05 15:44:25 +0100

    Align our repo to recent changes to the hmpps-template-typescript (#76)

--- server/data/index.ts
+++ server/data/index.ts
@@ -3,7 +3,7 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
-import { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
+import { AuthenticationClient, InMemoryTokenStore, RedisTokenStore } from '@ministryofjustice/hmpps-auth-clients'
 import { RestClient } from '@ministryofjustice/hmpps-rest-client'
 import { initialiseAppInsights, buildAppInsightsClient } from '../utils/azureAppInsights'
 import applicationInfoSupplier from '../applicationInfo'
@@ -12,15 +12,10 @@ const applicationInfo = applicationInfoSupplier()
 initialiseAppInsights()
 buildAppInsightsClient(applicationInfo)
 
-import HmppsAuthClient from './hmppsAuthClient'
 import { createRedisClient } from './redisClient'
-import RedisTokenStore from './tokenStore/redisTokenStore'
-import InMemoryTokenStore from './tokenStore/inMemoryTokenStore'
 import config from '../config'
 import HmppsAuditClient from './hmppsAuditClient'
 import logger from '../../logger'
-
-type RestClientBuilder<T> = (token: string) => T
 
 export const dataAccess = () => {
   const hmppsAuthClient = new AuthenticationClient(
@@ -39,4 +34,4 @@ export const dataAccess = () => {
 
 export type DataAccess = ReturnType<typeof dataAccess>
 
-export { HmppsAuthClient, RestClientBuilder, HmppsAuditClient }
+export { AuthenticationClient, HmppsAuditClient }
