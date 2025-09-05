959381bfd56ac8cbc51d4f270fd9aca9caf8a4c9
Ben Carter <ben.carter@justice.gov.uk>
2025-09-05 10:51:52 +0100

    Populate location data report with additional fields

--- server/types/entities/deviceActivation.ts
+++ server/types/entities/deviceActivation.ts
@@ -1,9 +1,12 @@
 type DeviceActivation = {
   deviceActivationId: number
   deviceId: number
+  deviceName: string
   personId: number
   deviceActivationDate: string
   deviceDeactivationDate: string | null
+  orderStart: string
+  orderEnd: string
 }
 
 export default DeviceActivation
