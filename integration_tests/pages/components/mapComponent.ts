90aa400a30b94c69cf6330d8555f97dba865ccf4
Patrick ODonnell <patrick.odonnell@justice.gov.uk>
2025-10-23 09:39:50 +0100

    Remove map component specific tests and config (#95)

--- integration_tests/pages/components/mapComponent.ts
+++ integration_tests/pages/components/mapComponent.ts
@@ -1,6 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
 import Map from 'ol/Map'
-import BaseLayer from 'ol/layer/Base'
 import { PageElement } from '../page'
 import MapSidebarComponent from './mapSidebarComponent'
 
@@ -64,57 +63,5 @@ export default class MapComponent {
 
   shouldNotHaveAlerts() {
     return this.element.find('.moj-alert').should('have.length', 0)
-  }
-
-  shouldShowOverlay(): void {
-    cy.get('moj-map').shadow().find('.ol-overlay-container').should('be.visible')
-  }
-
-  shouldNotShowOverlay(): void {
-    cy.get('moj-map').shadow().find('.ol-overlay-container').should('not.be.visible')
-  }
-
-  shouldHaveMapLayer(layer: BaseLayer | undefined, name: string): void {
-    expect(layer, `${name} layer should exist`).to.not.equal(undefined)
-  }
-
-  triggerPointerEventsAt(coordinate: number[], map: Map): void {
-    cy.window().then(win => {
-      const canvas = map.getViewport().querySelector('canvas')
-      const rect = canvas.getBoundingClientRect()
-      const pixel = map.getPixelFromCoordinate(coordinate)
-      const clientX = rect.left + pixel[0]
-      const clientY = rect.top + pixel[1]
-
-      const events = ['pointerdown', 'pointerup', 'click']
-      events.forEach(type => {
-        const event = new win.PointerEvent(type, {
-          clientX,
-          clientY,
-          bubbles: true,
-          cancelable: true,
-          view: win,
-        })
-        canvas.dispatchEvent(event)
-      })
-    })
-  }
-
-  // Recursively searches through all OL layers/groups and sub-layers to find a layer with the given title
-  findLayerByTitle(map: Map, title: string): BaseLayer | undefined {
-    const walkLayers = (layer: BaseLayer): BaseLayer | undefined => {
-      if (layer.get('title') === title) return layer
-
-      const children =
-        (layer as unknown as { getLayers?: () => { getArray?: () => BaseLayer[] } }).getLayers?.().getArray?.() ?? []
-
-      for (const child of children) {
-        const matchedResult = walkLayers(child)
-        if (matchedResult) return matchedResult
-      }
-      return undefined
-    }
-
-    return walkLayers(map.getLayerGroup())
   }
 }
