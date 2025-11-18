85ff094dc6008d4525430d62ac995a37037c821b
Patrick O'Donnell <patrick.odonnell@justice.gov.uk>
2025-11-18 10:49:47 +0000

    Added date error to apply to time inputs and enable/disable Apply button on keyup event as well as change event

--- assets/js/forms/date-filter-form/index.ts
+++ assets/js/forms/date-filter-form/index.ts
@@ -3,53 +3,52 @@ import { queryElement, queryElementAll } from '../../utils/utils'
 const padDate = (input: HTMLInputElement) => {
   const value = input.value.trim()
 
-  // Match dates where day and month can be 1 or 2 digits:
-  // E.g. d/m/yyyy, dd/m/yyyy, d/mm/yyyy, or dd/mm/yyyy
+  // Match: d/m/yyyy | dd/m/yyyy | d/mm/yyyy | dd/mm/yyyy
   const match = value.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/)
   if (!match) return
 
   const [, dayMatched, monthMatched, year] = match
 
-  const day = dayMatched.padStart(2, '0')
-  const month = monthMatched.padStart(2, '0')
-
   // eslint-disable-next-line no-param-reassign
-  input.value = `${day}/${month}/${year}`
+  input.value = `${dayMatched.padStart(2, '0')}/${monthMatched.padStart(2, '0')}/${year}`
 }
 
 const initialiseDateFilterForm = () => {
   const form = queryElement(document, '#dateFilterForm', HTMLFormElement)
+  if (!form) return
 
-  if (form) {
-    const inputs = queryElementAll(form, 'input', HTMLInputElement)
-    const continueButton = queryElement(form, '#continue', HTMLButtonElement)
-    const resetButton = queryElement(form, '#reset', HTMLButtonElement)
+  const inputs = queryElementAll(form, 'input', HTMLInputElement)
+  const continueButton = queryElement(form, '#continue', HTMLButtonElement)
+  const resetButton = queryElement(form, '#reset', HTMLButtonElement)
 
-    const main = document.querySelector('main')
-    const pageType = main?.dataset.page
-    const isSubjectPage = pageType === 'subject'
+  const main = document.querySelector('main')
+  const pageType = main?.dataset.page
+  const isSubjectPage = pageType === 'subject'
 
-    continueButton.disabled = isSubjectPage
-    resetButton.disabled = isSubjectPage
+  continueButton.disabled = isSubjectPage
+  resetButton.disabled = isSubjectPage
 
-    inputs.forEach(input => {
-      input.addEventListener('change', () => {
-        continueButton.disabled = false
-        resetButton.disabled = false
-      })
-
-      if (input.classList.contains('moj-js-datepicker-input')) {
-        input.addEventListener('change', () => {
-          padDate(input)
-        })
-      }
-    })
-
-    form.addEventListener('reset', () => {
-      continueButton.disabled = isSubjectPage
-      resetButton.disabled = true
-    })
+  const enableButtons = () => {
+    continueButton.disabled = false
+    resetButton.disabled = false
   }
+
+  inputs.forEach(input => {
+    input.addEventListener('input', enableButtons)
+    input.addEventListener('change', enableButtons)
+
+    if (input.classList.contains('moj-js-datepicker-input')) {
+      input.addEventListener('change', () => {
+        padDate(input)
+        enableButtons()
+      })
+    }
+  })
+
+  form.addEventListener('reset', () => {
+    continueButton.disabled = isSubjectPage
+    resetButton.disabled = true
+  })
 }
 
 export default initialiseDateFilterForm
