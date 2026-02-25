c4810d5659d029aa2c128d5c07b68040790ba5a4
Ben Carter <ben.carter@justice.gov.uk>
2026-02-25 10:45:32 +0000

    Lint

--- server/controllers/policeData/ingestionAttempt.ts
+++ server/controllers/policeData/ingestionAttempt.ts
@@ -1,13 +1,10 @@
-import { RequestHandler, RequestParamHandler } from "express";
-import PoliceDataService from "../../services/policeDataService";
-
+import { RequestHandler } from 'express'
+import PoliceDataService from '../../services/policeDataService'
 
 export default class PoliceDataIngestionAttemptController {
-    constructor(private readonly policeDataService: PoliceDataService){}
+  constructor(private readonly policeDataService: PoliceDataService) {}
 
-    view: RequestHandler = async (req, res) => {
-        res.render('pages/policeData/ingestionAttempt', {
-
-        })
-    }
+  view: RequestHandler = async (req, res) => {
+    res.render('pages/policeData/ingestionAttempt', {})
+  }
 }
