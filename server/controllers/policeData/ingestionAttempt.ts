f778a62b60ef6cd32acca410ef1b6cba8ecb26b9
Ben Carter <ben.carter@justice.gov.uk>
2026-02-25 10:38:59 +0000

    Bootstrap the police data ingestion attempt view

---
+++ server/controllers/policeData/ingestionAttempt.ts
@@ -0,0 +1,13 @@
+import { RequestHandler, RequestParamHandler } from "express";
+import PoliceDataService from "../../services/policeDataService";
+
+
+export default class PoliceDataIngestionAttemptController {
+    constructor(private readonly policeDataService: PoliceDataService){}
+
+    view: RequestHandler = async (req, res) => {
+        res.render('pages/policeData/ingestionAttempt', {
+
+        })
+    }
+}
