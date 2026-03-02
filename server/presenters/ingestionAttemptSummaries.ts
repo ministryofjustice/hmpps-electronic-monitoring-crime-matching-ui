df4cf3e38291ab119ae140a7aeee06de020cbb06
Ben Carter <ben.carter@justice.gov.uk>
2026-03-02 10:45:00 +0000

    Add matchesText to view model

--- server/presenters/ingestionAttemptSummaries.ts
+++ server/presenters/ingestionAttemptSummaries.ts
@@ -1,10 +1,25 @@
 import IngestionAttemptSummary from '../types/ingestionAttemptSummary'
 import presentPoliceForceArea from './policeForceArea'
 
-const presentIngestionAttemptSummary = ({ policeForceArea, ...ingestionAttempt }: IngestionAttemptSummary) => {
+const failedIngestionStatuses = new Set(['FAILED', 'ERROR'])
+
+const getMatchesText = ({ ingestionStatus, matches }: IngestionAttemptSummary): string => {
+  if (failedIngestionStatuses.has(ingestionStatus)) {
+    return 'N/A'
+  }
+
+  if (matches === null) {
+    return 'In progress'
+  }
+
+  return matches.toString()
+}
+
+const presentIngestionAttemptSummary = (ingestionAttempt: IngestionAttemptSummary) => {
   return {
     ...ingestionAttempt,
-    policeForceArea: presentPoliceForceArea(policeForceArea),
+    matchesText: getMatchesText(ingestionAttempt),
+    policeForceArea: presentPoliceForceArea(ingestionAttempt.policeForceArea),
   }
 }
 
