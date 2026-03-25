9df29abf0fe907d259811617e79936568dd68f20
Ben Carter <ben.carter@justice.gov.uk>
2026-03-25 13:17:09 +0000

    Export validation errors

--- server/controllers/policeData/ingestionAttempt.ts
+++ server/controllers/policeData/ingestionAttempt.ts
@@ -2,11 +2,12 @@ import { RequestHandler } from 'express'
 import PoliceDataService from '../../services/policeDataService'
 import presentIngestionAttempt from '../../presenters/ingestionAttempt'
 import { policeDataIngestionAttemptQuerySchema } from '../../schemas/policeData/ingestionAttempt'
+import generateValidationErrorsExport from '../../presenters/reports/validationErrors'
 
 export default class PoliceDataIngestionAttemptController {
   constructor(private readonly policeDataService: PoliceDataService) {}
 
-  view: RequestHandler = async (req, res, next) => {
+  view: RequestHandler = async (req, res) => {
     const { username } = res.locals.user
     const { ingestionAttemptId } = req.params
     const { returnTo } = policeDataIngestionAttemptQuerySchema.parse(req.query)
@@ -16,5 +17,18 @@ export default class PoliceDataIngestionAttemptController {
       ingestionAttempt: presentIngestionAttempt(ingestionAttempt),
       backLink: returnTo,
     })
+  }
+
+  export: RequestHandler = async (req, res) => {
+    const { username } = res.locals.user
+    const { ingestionAttemptId } = req.params
+    const ingestionAttempt = await this.policeDataService.getIngestionAttempt(username, ingestionAttemptId)
+
+    const csvData = generateValidationErrorsExport(ingestionAttempt.validationErrors)
+    const fileName = `validation-errors-${ingestionAttempt.batchId}.csv`
+
+    res.setHeader('Content-Type', 'text/csv')
+    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`)
+    res.send(csvData)
   }
 }
