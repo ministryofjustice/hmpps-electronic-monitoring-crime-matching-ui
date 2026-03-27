d5e77e4451ea45ed96d268cae7b06fba8ea9d107
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2026-03-27 14:56:56 +0000

    Removing unused type

--- server/presenters/proximityAlert/mapPositions.ts
+++ server/presenters/proximityAlert/mapPositions.ts
@@ -19,33 +19,6 @@ export type ProximityAlertMapPosition =
       longitude: number
     }
 
-export type MatchingResultFixture = {
-  crimeVersion: {
-    crimeVersionId: string
-    crimeReference: string
-    crimeType: string
-    fromDateTime: string
-    toDateTime: string
-    latitude: number
-    longitude: number
-    crimeText: string
-  }
-  matchedDeviceWearers: Array<{
-    deviceWearerId: string
-    name: string
-    nomisId: string
-    deviceId: number
-    positions: Array<{
-      positionId: string
-      capturedDateTime: string
-      sequenceLabel: string
-      latitude: number
-      longitude: number
-      confidenceCircle: number
-    }>
-  }>
-}
-
 export default function toProximityAlertMapPositions(crimeVersion: CrimeVersion): ProximityAlertMapPosition[] {
   const crimePosition: ProximityAlertMapPosition = {
     positionType: 'crime',
