151c88988bdd4b288d8f6cf54cf834336d10a5c6
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2026-05-07 08:54:09 +0100

    Toggle authorising manager question visibility (#215)

--- assets/js/components/index.ts
+++ assets/js/components/index.ts
@@ -1,7 +1,8 @@
 import MapFocusOnLayer from './map-focus-on-layer'
 import MapLayerVisibilityToggle from './map-layer-visibility-toggle'
+import RevealableContent from './revealable-content'
 import SelectAll from './select-all'
 
-const components = [MapLayerVisibilityToggle, MapFocusOnLayer, SelectAll]
+const components = [MapLayerVisibilityToggle, MapFocusOnLayer, RevealableContent, SelectAll]
 
 export default components
