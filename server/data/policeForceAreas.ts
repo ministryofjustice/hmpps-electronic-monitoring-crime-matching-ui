ebab614a88ffb56f2e2a2c1c5f1eaba5e5c751dc
Ben Carter <ben.carter@justice.gov.uk>
2026-09-08 12:23:43 +0100

    Define police force areas in a single place for consistency

--- server/data/policeForceAreas.ts
+++ server/data/policeForceAreas.ts
@@ -1,23 +1,35 @@
-const policeForceAreas = new Set([
-  'AVON_AND_SOMERSET',
-  'BEDFORDSHIRE',
-  'CHESHIRE',
-  'CITY_OF_LONDON',
-  'CUMBRIA',
-  'DERBYSHIRE',
-  'DURHAM',
-  'ESSEX',
-  'GLOUCESTERSHIRE',
-  'GWENT',
-  'HAMPSHIRE',
-  'HERTFORDSHIRE',
-  'HUMBERSIDE',
-  'KENT',
-  'METROPOLITAN',
-  'NORTH_WALES',
-  'NOTTINGHAMSHIRE',
-  'SUSSEX',
-  'WEST_MIDLANDS',
+const policeForceAreas: Map<string, string> = new Map([
+  ['AVON_AND_SOMERSET', 'Avon and Somerset'],
+  ['BEDFORDSHIRE', 'Bedfordshire'],
+  ['CHESHIRE', 'Cheshire'],
+  ['CITY_OF_LONDON', 'City of London'],
+  ['CUMBRIA', 'Cumbria'],
+  ['DERBYSHIRE', 'Derbyshire'],
+  ['DURHAM', 'Durham'],
+  ['ESSEX', 'Essex'],
+  ['GLOUCESTERSHIRE', 'Gloucestershire'],
+  ['GWENT', 'Gwent'],
+  ['HAMPSHIRE', 'Hampshire'],
+  ['HERTFORDSHIRE', 'Hertfordshire'],
+  ['HUMBERSIDE', 'Humberside'],
+  ['KENT', 'Kent'],
+  ['METROPOLITAN', 'Metropolitan'],
+  ['NORTH_WALES', 'North Wales'],
+  ['NOTTINGHAMSHIRE', 'Nottinghamshire'],
+  ['SUSSEX', 'Sussex'],
+  ['WEST_MIDLANDS', 'West Midlands'],
 ])
 
-export default policeForceAreas
+// Police force areas formatted as govuk select items - https://github.com/alphagov/govuk-frontend/blob/main/packages/govuk-frontend/src/govuk/components/select/select.yaml#L10
+const policeForceAreaOptions = [
+  {
+    value: '',
+    text: '',
+  },
+  Object.keys(policeForceAreas).map(key => ({
+    value: key,
+    text: policeForceAreas.get(key) || '',
+  })),
+]
+
+export { policeForceAreas, policeForceAreaOptions }
